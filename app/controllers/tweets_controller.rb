9e81b103dc2f06907c46a5816e979462b38eb18c
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 02:40:09 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,34 +3,31 @@ class TweetsController < ApplicationController
   set :views, Proc.new { File.join(root, "../views/") }
 
   get '/tweets' do
-<<<<<<< HEAD
+
     if !Helpers.is_logged_in?(session)
       redirect to '/login'
     end
-=======
->>>>>>> e498e2874dc1c65b9572a3cc6e96705b3cab98ce
+
      @tweets = Tweet.all
      erb :'tweets/tweets'
   end
 
   get '/tweets/new' do
-<<<<<<< HEAD
+
     if !Helpers.is_logged_in?(session)
       redirect to '/login'
     end
-=======
->>>>>>> e498e2874dc1c65b9572a3cc6e96705b3cab98ce
+
     @error = ""
     erb :'tweets/new'
   end
 
   get '/tweets/:id' do
-<<<<<<< HEAD
+
     if !Helpers.is_logged_in?(session)
       redirect to '/login'
     end
-=======
->>>>>>> e498e2874dc1c65b9572a3cc6e96705b3cab98ce
+
     @tweet = Tweet.find_by(id: params[:id])
     erb :'tweets/show_tweet'
   end
@@ -38,12 +35,11 @@ class TweetsController < ApplicationController
 
 
   post '/tweets' do
-<<<<<<< HEAD
+
     if !Helpers.is_logged_in?(session)
       redirect to '/login'
     end
-=======
->>>>>>> e498e2874dc1c65b9572a3cc6e96705b3cab98ce
+
     if params[:content] == ""
       @error = "Tweets can't be blank"
       erb :'tweets/new'
@@ -54,24 +50,22 @@ class TweetsController < ApplicationController
   end
 
   get '/tweets/:id/edit' do
-<<<<<<< HEAD
+
     if !Helpers.is_logged_in?(session)
       redirect to '/login'
     end
-=======
->>>>>>> e498e2874dc1c65b9572a3cc6e96705b3cab98ce
+
     @tweet = Tweet.find_by(id: params[:id])
     @error = ""
     erb :'tweets/edit_tweet'
   end
 
   post '/tweets/:id' do
-<<<<<<< HEAD
+
     if !Helpers.is_logged_in?(session)
       redirect to '/login'
     end
-=======
->>>>>>> e498e2874dc1c65b9572a3cc6e96705b3cab98ce
+
     @tweet = Tweet.find_by(id: params[:id])
     if params[:content] == ""
       @error = "Can't edit a tweet to have no content"
@@ -83,12 +77,11 @@ class TweetsController < ApplicationController
   end
 
   post '/tweets/:id/delete' do
-<<<<<<< HEAD
+
     if !Helpers.is_logged_in?(session)
       redirect to '/login'
     end
-=======
->>>>>>> e498e2874dc1c65b9572a3cc6e96705b3cab98ce
+
     tweet = Tweet.find_by(id: params[:id])
     tweet.destroy
     redirect to '/tweets'
